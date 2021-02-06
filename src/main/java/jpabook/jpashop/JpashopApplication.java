3b24f3a82140a92669a3ab91fd36fff3c2974006
hwangyoungjin <innovation950302@gmail.com>
2021-02-07 02:47:46 +0900

    환경설정 테스트 완료

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -3,11 +3,12 @@ package jpabook.jpashop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 @SpringBootApplication
 public class JpashopApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(JpashopApplication.class, args);
     }
 
